cc350a3e1250177c7f8a6370d9b6dc8b8f35ff20
ducojoek <84350199+ducojoek@users.noreply.github.com>
2024-12-05 11:02:12 +0100

    spatie

--- src/morse_code/morse_code.py
+++ src/morse_code/morse_code.py
@@ -25,41 +25,51 @@ list_text = [
     "x",
     "y",
     "z",
+    " ",
+    "  ",
 ]
 list_morse = [
-    "01",
-    "1000",
-    "1010",
-    "100",
-    "0",
-    "1000",
-    "001",
-    "0000",
-    "00",
-    "1000",
-    "101",
-    "0100",
-    "11",
-    "10",
-    "111",
-    "0110",
-    "1101",
-    "010",
-    "000",
+    "12",
+    "2111",
+    "2121",
+    "211",
     "1",
-    "001",
-    "0001",
-    "011",
-    "0110",
-    "0100",
-    "0011",
+    "2111",
+    "112",
+    "1111",
+    "11",
+    "2111",
+    "212",
+    "1211",
+    "22",
+    "21",
+    "222",
+    "1221",
+    "2212",
+    "121",
+    "111",
+    "2",
+    "112",
+    "1112",
+    "122",
+    "1221",
+    "1211",
+    "1122",
+    "3",
+    "33",
 ]
 
 
 def translate_text_to_morse(text):
     position = list_text.index(text)
     morse = list_morse[position]
-    print(morse)
+    return morse
+
+
+def translate_morse_to_text(morse):
+    position = list_morse.index(morse)
+    text = list_text[position]
+    return text
 
 
 # translate_text_to_morse("d")
@@ -69,7 +79,8 @@ def word_to_morse(word):
     for unit in range(0, len(woord)):
         letter_in_morse = translate_text_to_morse(woord[unit])
         signaal.append(letter_in_morse)
+    print(signaal)
     return signaal
 
 
-word_to_morse("hello")
+word_to_morse("hello world")
