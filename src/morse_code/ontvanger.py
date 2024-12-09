b73c31c2e233f27db32263d68a8c4cdd4614a293
SilvijnHasberg <hasbergsilvijn@gmail.com>
2024-12-09 14:34:51 +0100

    ontvanger

--- src/morse_code/ontvanger.py
+++ src/morse_code/ontvanger.py
@@ -1,60 +1,66 @@
 import time
+
 from morse_code.arduino_controller import ArduinoVISADevice
 
-lamp = ArduinoVISADevice(ports=)
+lamp = ArduinoVISADevice(ports="ASRL12::INSTR")
+
+crucial_value = 970
 
 
 def word():
     # functie ontvangt lichtsignalen en vertaalt deze naar dots(1), dash(2), en volgende letter(3).
-    # en returned een lijst met 
+    # en returned een lijst met
 
-
+    word = str(0)
+    word_list = []
     space = 0
-    crucial_value = 0
     above_crucial_count = 0
     below_crucial_count = 0
     one_three = 0
 
-    dot_len = 5
-    dash_len = 10
-    let_len = 30
-    space_len = 60
-
+    dot_len = 10
+    dash_len = 30
+    let_len = 20
+    space_len = 40
 
     while space == 0:
-        U = int(lamp.get_input_value(channel= 1)) - int(lamp.get_input_value(channel=2))
-
-        if U > crucial_value:
+        U = int(lamp.get_input_value(channel=1)) - int(lamp.get_input_value(channel=2))
+        print(U)
+        if U < crucial_value:
             above_crucial_count += 1
             below_crucial_count = 0
             one_three = 0
 
-        if U < crucial_value:
-            below_crucial_count +=1
+        if U > crucial_value:
+            below_crucial_count += 1
 
-            if dot_len <= above_crucial_count >= dash_len:
+            if dot_len <= above_crucial_count <= dash_len:
                 word += str(1)
+                print("dot")
 
-            if above_crucial_count>= dash_len:
+            if above_crucial_count >= dash_len:
                 word += str(2)
+                print("dash")
 
             if below_crucial_count >= let_len and one_three < 1:
-                word += str(3)
+                one_three += 1
 
             if below_crucial_count >= space_len:
                 return word
-            
+
             above_crucial_count = 0
 
-        time.pause(0.01)
+        time.sleep(0.05)
 
 
-def translater():
-
-
-
-    
-            
-
-            
-            
+t = 0
+let_list = []
+while t < 10000:
+    if (
+        int(lamp.get_input_value(channel=1)) - int(lamp.get_input_value(channel=2))
+        < crucial_value
+    ):
+        let_list.append(word())
+        print(let_list)
+    time.sleep(0.01)
+    t += 1
--- src/morse_code/test.py
+++ src/morse_code/test.py
@@ -1,8 +1,14 @@
+import time
 
+import pyvisa
 
+from morse_code.arduino_controller import ArduinoVISADevice
 
-lijst =  str(1)
-lijst += str(2)
-
-
-print(int(lijst))
+lamp = ArduinoVISADevice(ports="ASRL12::INSTR")
+lamp.set_output_value(value=1023)
+t = 0
+while t < 10000:
+    val = int(lamp.get_input_value(channel=1)) - int(lamp.get_input_value(channel=2))
+    print(val)
+    t += 1
+    time.sleep(0.1)
